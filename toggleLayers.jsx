539731046560c2b04041d54b2293dcadc89e4e3e
X8J <37266311+X8J@users.noreply.github.com>
2024-02-27 19:35:02 -0500

    Update toggleLayers.jsx

--- toggleLayers.jsx
+++ toggleLayers.jsx
@@ -1,63 +1,36 @@
-(function(thisObj) {
-    scriptBuildUI(thisObj)
-    function scriptBuildUI(thisObj) {
-        var win = (thisObj instanceof Panel) ? thisObj : new Window('palette', "Toggle Solo", undefined, {
-            resizeable: true
-        });
-        
-var dropdown = win.add("dropdownlist", undefined, ["Video Layers", "Adjustment Layers", "Solid Layers", "MP3 Audio Layers",]);
-var toggleButton = win.add("button", undefined, "Toggle Solo"); //toggle button
-toggleButton.onClick = toggleSoloAndAdjustVisibility;
-var invertButton = win.add("button", undefined, "Invert"); //invert button
-var invertEnabled = false;
-invertButton.onClick = toggleInversion; 
-
-win.orientation = "column"; 
-win.alignChildren = "fill";
-win.center();
-win.show();
-
-
-
-function toggleSoloAndAdjustVisibility() {
-    //get comp
+function renameAdjustmentLayers() {
     var comp = app.project.activeItem;
     if (comp && comp instanceof CompItem) {
-       
-        for (var i = 1; i <= comp.numLayers; i++) {
-            var layer = comp.layer(i);
-            
-            if (dropdown.selection.text === "Video Layers" && (invertEnabled ? !layer.hasVideo : layer.hasVideo)) {
-                
-                layer.solo = !layer.solo;
-
-            } else if (dropdown.selection.text === "Adjustment Layers" && (invertEnabled ? !layer.adjustmentLayer : layer.adjustmentLayer)) {
-                
-                layer.solo = !layer.solo;
-
-            } else if (dropdown.selection.text === "Solid Layers" && (invertEnabled ? !layer.source.mainSource instanceof SolidSource : layer.source.mainSource instanceof SolidSource)){
-
-                layer.solo = !layer.solo;
-
-            } else if (dropdown.selection.text === "MP3 Audio Layers" && (invertEnabled ? !layer.hasAudio : layer.hasAudio)) {
-                
-                layer.solo = !layer.solo;
-                
+        var layers = comp.layers;
+        for (var i = 1; i <= layers.length; i++) {
+            var layer = layers[i];
+            if (layer instanceof AVLayer && layer.adjustmentLayer) {
+                var effects = layer.property("ADBE Effect Parade");
+                if (effects.numProperties > 0) {
+                    var newName = "";
+                    for (var j = 1; j <= effects.numProperties; j++) {
+                        var effect = effects.property(j);
+                        var effectName = effect.name;
+                        newName += effectName + "+";
+                    }
+                    layer.name = newName;
+                }
             }
         }
+        alert("Renamed");
     } else {
-        alert("Please open a composition to use this script.");
+        alert("Composition not found");
     }
 }
 
-
-function toggleInversion() {
-    invertEnabled = !invertEnabled;
-    //change text
-    invertButton.text = invertEnabled ? "Invert (ON)" : "Invert (OFF)";
+function createGUI() {
+    var window = new Window("palette", "Kai BetterAdjLayers");
+    window.orientation = "column";
+    var button = window.add("button", undefined, "Rename Layers");
+    button.onClick = function() {
+        renameAdjustmentLayers();
+    };
+    window.show();
 }
 
-
-
-    }
-})(this);
+createGUI();
